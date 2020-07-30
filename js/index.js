7d5b8d0db0902ea568d3b42d144e06d8130c19bc
Louis Daza <louisdaza@hotmail.com>
2020-07-30 07:22:40 -0700

    fixed excess html

--- js/index.js
+++ js/index.js
@@ -1,183 +1,164 @@
-
-let animateActions = "restart complete resume none";
-    let startPt = "left 50%";
-    let endPt = "center 50%";
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#test-slides-1',
-            horizontal: true,
-            scroller: "#wheel",
-            start: startPt,
-            end: endPt,
-            toggleActions: animateActions,
-            markers: true,
-            scrub: 1
-        }
-    })
-    
-    tl.fromTo('#test6', 0.4, {top: "50%", opacity: "1"}, {top: "60%", opacity:"0", ease: Power2.easeInOut})
-    .fromTo('#test', 0.5, {top: "40%", opacity: "0"}, {top: "50%", opacity:"1", ease: Power2.easeInOut});
-
-    let tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#test-slides-2',
-            horizontal: true,
-            scroller: "#wheel",
-            start: startPt,
-            end: endPt,
-            toggleActions: animateActions,
-            scrub: 1
-        }
-    })
-    
-    tl2.fromTo('#test', 1, {top: "50%", opacity: "1"}, {top: "60%", opacity:"0", ease: Power2.easeInOut})
-    .fromTo('#test2', 1, {top: "40%", opacity: "0"}, {top: "50%", opacity:"1", ease: Power2.easeInOut}, "<");
-    let tl3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#test-slides-3',
-            horizontal: true,
-            scroller: "#wheel",
-            start: startPt,
-            end: endPt,
-            toggleActions: animateActions,
-            scrub: 1
-        }
-    })
-    
-    tl3.fromTo('#test2', 1, {top: "50%", opacity: "1"}, {top: "60%", opacity:"0", ease: Power2.easeInOut})
-    .fromTo('#test3', 1, {top: "40%", opacity: "0"}, {top: "50%", opacity:"1", ease: Power2.easeInOut}, "<");
-
-    let tl4 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#test-slides-4',
-            horizontal: true,
-            scroller: "#wheel",
-            start: startPt,
-            end: endPt,
-            toggleActions: animateActions,
-            scrub: 1
-        }
-    })
-    
-    tl4.fromTo('#test3', 1, {top: "50%", opacity: "1"}, {top: "60%", opacity:"0", ease: Power2.easeInOut})
-    .fromTo('#test4', 1, {top: "40%", opacity: "0"}, {top: "50%", opacity:"1", ease: Power2.easeInOut}, "<");
-
-    let tl5 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#test-slides-5',
-            horizontal: true,
-            scroller: "#wheel",
-            start: startPt,
-            end: endPt,
-            toggleActions: animateActions,
-            scrub: 1
-        }
-    })
-    
-    tl5.fromTo('#test4', 1, {top: "50%", opacity: "1"}, {top: "60%", opacity:"0", ease: Power2.easeInOut})
-    .fromTo('#test5', 1, {top: "40%", opacity: "0"}, {top: "50%", opacity:"1", ease: Power2.easeInOut}, "<");
-
-    let tl6 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#test-slides-6',
-            horizontal: true,
-            scroller: "#wheel",
-            start: startPt,
-            end: endPt,
-            toggleActions: animateActions,
-            scrub: 1
-        }
-    })
-    
-    tl6.fromTo('#test5', 1, {top: "50%", opacity: "1"}, {top: "60%", opacity:"0", ease: Power2.easeInOut})
-    .fromTo('#test6', 1, {top: "40%", opacity: "0"}, {top: "50%", opacity:"1", ease: Power2.easeInOut}, "<");
-
-   
-    window.addEventListener('DOMContentLoaded', (event) => {
-        let slidezz = document.getElementsByClassName('slideshow-title');
-        for(let i = 0; i < slidezz.length; i++){
-            slidezz[i].style.opacity = '0';
-        }
-        //slidezz[5].style.opacity = '0';
+var e,
+  t = require("three"),
+  i =
+    (e = require("gsap/TweenMax")) && "object" == typeof e && "default" in e
+      ? e.default
+      : e;
+module.exports = function (e) {
+  function n() {
+    for (var e = arguments, t = 0; t < arguments.length; t++)
+      if (void 0 !== e[t]) return e[t];
+  }
+  console.log(
+    "%c Hover effect by Robin Delaporte: https://github.com/robin-dela/hover-effect ",
+    "color: #bada55; font-size: 0.8rem"
+  );
+  var r = e.parent,
+    o = e.displacementImage,
+    a = e.image1,
+    s = e.image2,
+    l = n(e.imagesRatio, 1),
+    f = n(e.intensity1, e.intensity, 1),
+    d = n(e.intensity2, e.intensity, 1),
+    u = n(e.angle, Math.PI / 4),
+    v = n(e.angle1, u),
+    m = n(e.angle2, 3 * -u),
+    c = n(e.speedIn, e.speed, 1.6),
+    p = n(e.speedOut, e.speed, 1.2),
+    g = n(e.hover, !0),
+    h = n(e.easing, Expo.easeOut),
+    y = n(e.video, !1);
+  if (r)
+    if (a && s && o) {
+      var F = new t.Scene(),
+        x = new t.OrthographicCamera(
+          r.offsetWidth / -2,
+          r.offsetWidth / 2,
+          r.offsetHeight / 2,
+          r.offsetHeight / -2,
+          1,
+          1e3
+        );
+      x.position.z = 1;
+      var w = new t.WebGLRenderer({ antialias: !1, alpha: !0 });
+      w.setPixelRatio(2),
+        w.setClearColor(16777215, 0),
+        w.setSize(r.offsetWidth, r.offsetHeight),
+        r.appendChild(w.domElement);
+      var L = function () {
+          w.render(F, x);
+        },
+        H = new t.TextureLoader();
+      H.crossOrigin = "";
+      var W,
+        V,
+        E = H.load(o, L);
+      if (((E.magFilter = E.minFilter = t.LinearFilter), y)) {
+        var P = function () {
+          requestAnimationFrame(P), w.render(F, x);
+        };
+        P(),
+          ((y = document.createElement("video")).autoplay = !0),
+          (y.loop = !0),
+          (y.src = a),
+          y.load();
+        var M = document.createElement("video");
+        (M.autoplay = !0), (M.loop = !0), (M.src = s), M.load();
+        var U = new t.VideoTexture(y),
+          C = new t.VideoTexture(M);
+        (U.magFilter = C.magFilter = t.LinearFilter),
+          (U.minFilter = C.minFilter = t.LinearFilter),
+          M.addEventListener(
+            "loadeddata",
+            function () {
+              M.play(),
+                ((C = new t.VideoTexture(M)).magFilter = t.LinearFilter),
+                (C.minFilter = t.LinearFilter),
+                (b.uniforms.texture2.value = C);
+            },
+            !1
+          ),
+          y.addEventListener(
+            "loadeddata",
+            function () {
+              y.play(),
+                ((U = new t.VideoTexture(y)).magFilter = t.LinearFilter),
+                (U.minFilter = t.LinearFilter),
+                (b.uniforms.texture1.value = U);
+            },
+            !1
+          );
+      } else
+        (U = H.load(a, L)),
+          (C = H.load(s, L)),
+          (U.magFilter = C.magFilter = t.LinearFilter),
+          (U.minFilter = C.minFilter = t.LinearFilter);
+      var R = l;
+      r.offsetHeight / r.offsetWidth < R
+        ? ((W = 1), (V = r.offsetHeight / r.offsetWidth / R))
+        : ((W = (r.offsetWidth / r.offsetHeight) * R), (V = 1));
+      var b = new t.ShaderMaterial({
+          uniforms: {
+            intensity1: { type: "f", value: f },
+            intensity2: { type: "f", value: d },
+            dispFactor: { type: "f", value: 0 },
+            angle1: { type: "f", value: v },
+            angle2: { type: "f", value: m },
+            texture1: { type: "t", value: U },
+            texture2: { type: "t", value: C },
+            disp: { type: "t", value: E },
+            res: {
+              type: "vec4",
+              value: new t.Vector4(r.offsetWidth, r.offsetHeight, W, V),
+            },
+            dpr: { type: "f", value: window.devicePixelRatio },
+          },
+          vertexShader:
+            "\nvarying vec2 vUv;\nvoid main() {\n  vUv = uv;\n  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n}\n",
+          fragmentShader:
+            "\nvarying vec2 vUv;\n\nuniform float dispFactor;\nuniform float dpr;\nuniform sampler2D disp;\n\nuniform sampler2D texture1;\nuniform sampler2D texture2;\nuniform float angle1;\nuniform float angle2;\nuniform float intensity1;\nuniform float intensity2;\nuniform vec4 res;\nuniform vec2 parent;\n\nmat2 getRotM(float angle) {\n  float s = sin(angle);\n  float c = cos(angle);\n  return mat2(c, -s, s, c);\n}\n\nvoid main() {\n  vec4 disp = texture2D(disp, vUv);\n  vec2 dispVec = vec2(disp.r, disp.g);\n\n  vec2 uv = 0.5 * gl_FragCoord.xy / (res.xy) ;\n  vec2 myUV = (uv - vec2(0.5))*res.zw + vec2(0.5);\n\n\n  vec2 distortedPosition1 = myUV + getRotM(angle1) * dispVec * intensity1 * dispFactor;\n  vec2 distortedPosition2 = myUV + getRotM(angle2) * dispVec * intensity2 * (1.0 - dispFactor);\n  vec4 _texture1 = texture2D(texture1, distortedPosition1);\n  vec4 _texture2 = texture2D(texture2, distortedPosition2);\n  gl_FragColor = mix(_texture1, _texture2, dispFactor);\n}\n",
+          transparent: !0,
+          opacity: 1,
+        }),
+        D = new t.PlaneBufferGeometry(r.offsetWidth, r.offsetHeight, 1),
+        _ = new t.Mesh(D, b);
+      F.add(_),
+        g &&
+          (r.addEventListener("mouseenter", z),
+          r.addEventListener("touchstart", z),
+          r.addEventListener("mouseleave", S),
+          r.addEventListener("touchend", S)),
+        window.addEventListener("resize", function (e) {
+          r.offsetHeight / r.offsetWidth < R
+            ? ((W = 1), (V = r.offsetHeight / r.offsetWidth / R))
+            : ((W = (r.offsetWidth / r.offsetHeight) * R), (V = 1)),
+            (_.material.uniforms.res.value = new t.Vector4(
+              r.offsetWidth,
+              r.offsetHeight,
+              W,
+              V
+            )),
+            w.setSize(r.offsetWidth, r.offsetHeight),
+            L();
+        }),
+        (this.next = z),
+        (this.previous = S);
+    } else console.warn("One or more images are missing");
+  else console.warn("Parent missing");
+  function z() {
+    i.to(b.uniforms.dispFactor, c, {
+      value: 1,
+      ease: h,
+      onUpdate: L,
+      onComplete: L,
     });
-
-    //imgSlides will refer to the DOM element .mySlides--img
-    //it gets declared later on in the click initialization function
-
-    
-    let imgSlides;
-    let numSlides = document.getElementsByClassName('mySlides--img').length;
-    
-    //
-    
-    $('.mySlides--img').each(function(index){
-        let switchTL = new TimelineLite();
-        index++;
-        let slideIndex = '#slide--' + index;
-        switchTL.to('.slideshow-wheel', 0.5, {y: 200,opacity: 0})
-        .to('.slideshow-title', 0.5, {y: 200,opacity: 0}, "<")
-        .to('.slideshow-swipe', 0.5, {y: 200,opacity: 0}, "<")
-        .to(slideIndex, 0.5, {opacity: 1, y: 15, zIndex: 5, display: 'block'}).reversed(true);
-        this.animation = switchTL;
-    })
-    $(".mySlides--img").click(function(){
-        imgSlides = this;
-  this.animation.reversed(!this.animation.reversed());
+  }
+  function S() {
+    i.to(b.uniforms.dispFactor, p, {
+      value: 0,
+      ease: h,
+      onUpdate: L,
+      onComplete: L,
     });
-    $(".slide__back").click(function(){
-        imgSlides.animation.reversed(!imgSlides.animation.reversed());
-    });
-    
-    //let tlSlideIn = gsap.to('#')
-
-    let containerz = document.getElementById("wheel");
-    let containerWidth;
-    let segment; //one slide in the slide carousel
-    //////////////////////////////////
-    /////Change words on title
-    //////////////
-    let body = document.querySelector('body');
-    let title = document.getElementsByClassName('slideshow-title');
-
-    //slides
-    let slidez = document.getElementsByClassName('mySlides');
-    let imgGap = 0;
-    
-    //set up functions
-    function setFinal(){
-        segment = (containerWidth)/6;
-        console.log('segment is', segment);
-        //debug
-        console.log('scroll width:' ,containerz.scrollWidth);
-    }
-    function setWheelClone(){
-        $('#wheel').ready(function(){
-        containerWidth = containerz.scrollWidth;
-        
-        let clone=$("#wheel").contents().clone();
-
-        console.log('before width: ', containerWidth);
-        clone.appendTo("#wheel");
-        
-        imgGap = slidez[0].getBoundingClientRect().x;
-        $('#wheel').scroll(function(){
-            let scrollWindowPos = $('#wheel').scrollLeft();
-            if(scrollWindowPos >= (containerWidth + imgGap + 5)){
-                $('#wheel').scrollLeft(1)
-                ScrollTrigger.refresh();
-            }
-            if(scrollWindowPos <= 0){
-                $('#wheel').scrollLeft(containerWidth + imgGap);
-            }
-        })
-        
-    })
-    };
-
-    setWheelClone();
-    setTimeout(setFinal, 1500);
-    //setTimeout(scrollSpy, 1505);
-    ///////////////////////////////////
-    //Check for changes in device width
-    ////
-    window.addEventListener('resize', setFinal);
+  }
+};
